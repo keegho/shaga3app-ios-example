c19836c17b1a3c55bec26f5802b85f8c3fbfcdc1
Kegham Karsian <kegham1983@gmail.com>
2020-01-05 18:44:24 +0200

    Latest update with users

--- Shaga3 Web/ViewController.swift
+++ Shaga3 Web/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
    // let signature = "3f75148a3cc42d2a086118d53b62983ea71b6a92a1091e3525cf483bae26c482" //Get from degla server
    // let sherifSignature = "96482ab7d8ba729c942c30d6b144dd7abd5afd4b13ec03d0e0778f9b1ba03d63"
    // let guestSignature = "e20d6020cf3659c9cce3c738578fc10782aea1ba11612dd78d98a2b44f044f10"
+    
     let key = "degla" //Predefined key
 
     @IBOutlet weak var webView: WKWebView!
@@ -34,10 +35,7 @@ class ViewController: UIViewController {
                webView!.uiDelegate = self
             //WKWebView.clean()
             view = webView
-        
-//        self.navigationItem.title = "COMPETITION"
-//        self.title = "COMPETITION"
-//        self.tabBarController?.tabBar.items?[2].title = "COMPETITION"
+
         
         
     }
@@ -51,30 +49,8 @@ class ViewController: UIViewController {
        // let deglaUser = User(name: "Vahan", uuid: "31531e13fgea") //Degla User
        // let sherifDegla = User(name: "sherif", uuid: "346grs")
         //let guestUser = User(name: "guest", uuid: "guest")
-        DispatchQueue.main.async {
-        if #available(iOS 13.0, *) {
-           
-                self.webView.setupActivityIndicator(style: .large, color: .yellow)
-                      } else {
-                        self.webView.setupActivityIndicator(style: .whiteLarge, color: .yellow)
-                  }
-            }
-       
-
-       // webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
-       // webView.configuration.preferences.javaScriptEnabled = true
-        //webView.configuration.preferences.plugInsEnabled = true
- 
-         print(Constants.selectedUser.name)
-        let url = URL(string: "https://backend.shaga3app.com/api/authorize?user_name=\(Constants.selectedUser.name)&user_uuid=\(Constants.selectedUser.uuid)")
         
-        var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 15)
-        
-        request.setValue(Constants.selectedUser.signature, forHTTPHeaderField:"x-auth-signature")
-        request.setValue(key, forHTTPHeaderField:"x-shaga3app-id")
-        webView.load(request)
-        print(request)
-        print(request.allHTTPHeaderFields)
+        setWebView()
 
     }
     
@@ -107,13 +83,12 @@ class ViewController: UIViewController {
                 self.webView.setupActivityIndicator(style: .whiteLarge, color: .yellow)
              }
         }
-              
 
               // webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
               // webView.configuration.preferences.javaScriptEnabled = true
                //webView.configuration.preferences.plugInsEnabled = true
        
-               
+           print(Constants.selectedUser.name)
         let url = URL(string: "https://backend.shaga3app.com/api/authorize?user_name=\(Constants.selectedUser.name)&user_uuid=\(Constants.selectedUser.uuid)")
                
                var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 15)
@@ -145,7 +120,7 @@ extension ViewController: WKUIDelegate, WKNavigationDelegate {
         
         print(webView.url) //Check redirect after auth
         webView.activityIndicatorView.stopAnimating()
-        self.title = webView.title
+        //self.title = webView.title
     }
     
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
@@ -168,50 +143,6 @@ extension ViewController: WKUIDelegate, WKNavigationDelegate {
         
         webView.load(navigationAction.request)
         
-       // webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
-        // webView.configuration.preferences.plugInsEnabled = true
-       //  webView.configuration.preferences.javaScriptEnabled = true
-        
-        //if navigationAction.targetFrame?.isMainFrame == nil {
-       //     UIApplication.shared.open(navigationAction.request.url!)
-            
-        //    webView.load(navigationAction.request)
-      //  }
-       // return nil
-        
-      /*  if navigationAction.targetFrame == nil {
-            
-            let tempURL = navigationAction.request.url
-            var components = URLComponents()
-            components.scheme = tempURL?.scheme
-            components.host = tempURL?.host
-            components.path = tempURL!.path
-            
-            let webViewTemp = WKWebView(frame: self.webView.bounds, configuration: configuration)
-            webViewTemp.uiDelegate = self
-            webViewTemp.navigationDelegate = self
-            self.view.addSubview(webViewTemp)
-            
-//            print(tempURL?.path)
-//            print(tempURL?.scheme)
-//            print(tempURL)
-//            print(tempURL?.absoluteString)
-            
-          //  let newUrl = tempURL?.absoluteString.replacingOccurrences(of: "https://www.facebook.com/", with: "")
-         //   print(newUrl ?? "NO URL")
-            
-         //   UIApplication.shared.canOpenURL(URL(string: "fb://\(newUrl ?? "")")!)
-            
-        
-            
-            
-           // webViewTemp.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
-           // webViewTemp.configuration.preferences.plugInsEnabled = true
-          //  webViewTemp.configuration.preferences.javaScriptEnabled = true
-            
-            return webViewTemp
-        }*/
-        
         return nil
     }
     
@@ -224,22 +155,22 @@ extension ViewController: WKUIDelegate, WKNavigationDelegate {
         print("IM HERE IN DECIDE POLICY FOR ACTION")
         
         
-       /* if let host = navigationAction.request.url?.host {
-            if host.contains("facebook.com") || host.contains("whatsapp")  {
-                let newUrl = navigationAction.request.url?.absoluteString.replacingOccurrences(of: "https://www.facebook.com/", with: "")
-                print(newUrl ?? "NO URL")
-                //UIApplication.shared.open(URL(string: "fb://\(newUrl ?? "")")!)
-               // UIApplication.shared.canOpenURL(navigationAction.request.url!)
-                UIApplication.shared.open(navigationAction.request.url!)
+            if (navigationAction.request.url?.absoluteString.contains("www"))! {//if host.contains("apple")  {
+                if let url = navigationAction.request.url {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        // Earlier versions
+                        if UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.openURL(url)
+                        }
+                    }
+                }
+
                 decisionHandler(.cancel)
                 return
-            } //else if host.contains("shaga3app.com") {
-            //    decisionHandler(.allow)
-           // }
-        } */
-
+            }
         decisionHandler(.allow)
-       // self.dismiss(animated: true)
     }
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
